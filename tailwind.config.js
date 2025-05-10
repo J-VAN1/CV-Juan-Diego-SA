d475a09b9db5ed0e3a03b9325b0e1ebb37fb646e
J-VAN1 <juandiegosa2001@hotmail.com>
2025-05-09 22:42:21 -0600

    version 0.9.1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//     fontFamily: {
+//       'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+//       'serif': ['ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+//       'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
+//     }
+//   },
+//   plugins: [],
+// }
